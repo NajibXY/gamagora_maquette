0e5afc7998cdc12851eb69046a77d06c69be051e
NajibXY <naj.elkhadir@gmail.com>
2024-12-18 13:45:25 +0100

    y sorting and collision on upper cage

--- scripts/character.gd
+++ scripts/character.gd
@@ -7,7 +7,6 @@ const SPEED = 140.0
 const JUMP_VELOCITY = -10.0
 
 var animated_body: AnimatedSprite2D
-var player_node: Node2D
 var collision_node: CollisionShape2D
 
 
@@ -21,10 +20,8 @@ enum IdleModes {
 var idle_mode := IdleModes.RIGHT
 
 func _ready() -> void:
-	print($PlayerNode)
-	animated_body = $PlayerNode.get_node("CharacterCreatorToolImporter").get_node("AnimatedSprite")
+	animated_body = get_node("AnimatedSprite")
 	collision_node = get_node("CollisionShape2D")
-	player_node = get_node("PlayerNode")
 	animated_body.play("idle_down")
 
 func _physics_process(delta: float) -> void:
@@ -54,25 +51,30 @@ func _physics_process(delta: float) -> void:
 	# Handle movement input
 
 	handle_movement_input()
-	handle_velocity()
 
 	# move_and_slide()
 
 func handle_movement_input() -> void:
-	if Input.is_action_pressed("right"):
+	var dir = Input.get_vector("left", "right", "up", "down")
+	velocity = dir * SPEED
+
+	if velocity.x > 0:
 		animated_body.play("walk_right")
 		idle_mode = IdleModes.RIGHT
-	elif Input.is_action_pressed("left"):
+	elif velocity.x < 0:
 		animated_body.play("walk_left")
 		idle_mode = IdleModes.LEFT
-	elif Input.is_action_pressed("up"):
+		
+	if velocity.y < 0:
 		animated_body.play("walk_up")
 		idle_mode = IdleModes.UP
-	elif Input.is_action_pressed("down"):
+	elif velocity.y > 0:
 		animated_body.play("walk_down")
 		idle_mode = IdleModes.DOWN
 	else:
 		change_idle_animation()
+	
+	move_and_slide()
 	
 
 func change_idle_animation() -> void:
@@ -85,25 +87,11 @@ func change_idle_animation() -> void:
 			animated_body.play("idle_up")
 		IdleModes.DOWN:
 			animated_body.play("idle_down")
-
-func handle_velocity() -> void:
-	var direction := Input.get_action_strength("right") - Input.get_action_strength("left")
-	velocity.x = direction * SPEED
-	var direction_y := Input.get_action_strength("down") - Input.get_action_strength("up")
-	velocity.y = direction_y * SPEED
 	
-	move_and_slide()
+	
 
-	handle_positions()
 
-func handle_positions() -> void:
 
-	# todo refactor this to a better handling
-	collision_node.transform.origin = Vector2(transform.origin.x, transform.origin.y + 5)
-	player_node.transform.origin = transform.origin
-	print(transform.origin)
-	print("Collision pos " , collision_node.transform.origin)
-	print("Player pos " , player_node.transform.origin)
-	animated_body.transform.origin = transform.origin
+
 	
 	
